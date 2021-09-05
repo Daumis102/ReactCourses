ad58ae6d786d31166b158a6cc6773e18d081c9ba
Daumantas <daumis05@gmail.com>
2021-09-05 16:37:12 +0300

    60. community convention naming in reducers
    Describes that the action object in a reducer has to contain 'type' and a 'payload' named entries

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -9,21 +9,21 @@ const reducer = (state, action) => {
     // Reducer is used for maintaining the state of very similar variables which change in a clearly defined way
     // It has to always return a variable like its first arg, and the first arg can never be edited directly.
     // state === {red: Number, green: Number, blue:Number}; <- reminder of what the object looks like
-    // action === {colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15}
-
-    switch (action.colorToChange) {
-        case 'red':
-            if (state.red + action.amount > 255 || state.red + action.amount < 0)
+    // action === {type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15}
+    // By convention, action object contains a type which describes the action and a payload which is data needed for that action. Use this naming if you want other devs to understand you.
+    switch (action.type) {
+        case 'change_red':
+            if (state.red + action.payload > 255 || state.red + action.payload < 0)
                 return state;
-            return { ...state, red: state.red + action.amount }; // We don't want to change the state. Instead we recreate it from scratch with a new updated value.
-        case 'green':
-            if (state.green + action.amount > 255 || state.green + action.amount < 0)
+            return { ...state, red: state.red + action.payload }; // We don't want to change the state. Instead we recreate it from scratch with a new updated value.
+        case 'change_green':
+            if (state.green + action.payload > 255 || state.green + action.payload < 0)
                 return state;
-            return { ...state, green: state.green + action.amount };
-        case 'blue':
-            if (state.blue + action.amount > 255 || state.blue + action.amount < 0)
+            return { ...state, green: state.green + action.payload };
+        case 'change_blue':
+            if (state.blue + action.payload > 255 || state.blue + action.payload < 0)
                 return state;
-            return { ...state, blue: state.blue + action.amount };
+            return { ...state, blue: state.blue + action.payload };
         default:
             return state;
     }
@@ -36,16 +36,16 @@ const SquareScreen = () => {
 
     return <View>
         <ColorCounter
-            onIncrease={() => dispatch({ colorToChange: "red", amount: COLOR_INCREMENT })}  // dispatch actually just means 'runMyReducer' and takes in an object of action format defined in the reducer.
-            onDecrease={() => dispatch({ colorToChange: "red", amount: -COLOR_INCREMENT })}
+            onIncrease={() => dispatch({ type: "change_red", payload: COLOR_INCREMENT })}  // dispatch actually just means 'runMyReducer' and takes in an object of action format defined in the reducer.
+            onDecrease={() => dispatch({ type: "change_red", payload: -COLOR_INCREMENT })}
             color="Red" />
         <ColorCounter
-            onIncrease={() => dispatch({ colorToChange: "green", amount: COLOR_INCREMENT })}
-            onDecrease={() => dispatch({ colorToChange: "green", amount: -COLOR_INCREMENT })}
+            onIncrease={() => dispatch({ type: "change_green", payload: COLOR_INCREMENT })}
+            onDecrease={() => dispatch({ type: "change_green", payload: -COLOR_INCREMENT })}
             color="Green" />
         <ColorCounter
-            onIncrease={() => dispatch({ colorToChange: "blue", amount: COLOR_INCREMENT })}
-            onDecrease={() => dispatch({ colorToChange: "blue", amount: -COLOR_INCREMENT })}
+            onIncrease={() => dispatch({ type: "change_blue", payload: COLOR_INCREMENT })}
+            onDecrease={() => dispatch({ type: "change_blue", payload: -COLOR_INCREMENT })}
             color="Blue" />
         <View style={{
             height: 150,
