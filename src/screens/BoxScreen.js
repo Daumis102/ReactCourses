5267e3f5ca9c6a9821ed464655b5f6e17eca2db5
Daumantas <daumis05@gmail.com>
2021-12-08 20:45:02 +0200

    72. Flex Layout - AlignItems + remove redundant src

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const BoxScreen = () => {
+    // Main Box layout properties:
+    // margin
+    // marginVertical
+    // marginHorizontal
+    // padding
+    // paddingVertical
+    // paddingHorizontal
+    // borderWidth
+    return (<View style={styles.viewStyle}>
+        <Text style={styles.textStyle}>Box Screen 1</Text>
+        <Text style={styles.textStyle}>Box Screen 2</Text>
+        <Text style={styles.textStyle}>Box Screen 3</Text>
+    </View>);
+};
+
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderColor: 'black',
+        alignItems: 'flex-start'
+    },
+    textStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+    }
+});
+
+export default BoxScreen;
