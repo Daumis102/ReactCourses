b50edacd243a9423df460e1d2d3f76e4d46896be
daumantas <daumis05@gmail.com>
2021-12-30 16:34:20 +0200

    240. Add maps, ask user for location permission

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,8 +1,34 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import Map from '../components/Map';
+import { requestForegroundPermissionsAsync } from 'expo-location';
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 48 }}>Track Create Screen</Text>;
+  const [err, setErr] = useState(null);
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text h2>Create a Track</Text>
+      <Map></Map>
+      {err ? <Text>Please enable location services</Text> : null}
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({});
