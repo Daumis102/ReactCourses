d5a3295314d502a5899e060bf0aaf8d423af874f
daumantas <daumis05@gmail.com>
2021-12-30 16:57:23 +0200

    242. Use and read fake location data

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,15 +1,24 @@
+import '../_mockLocation';
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import Map from '../components/Map';
-import { requestForegroundPermissionsAsync } from 'expo-location';
+import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 
 const TrackCreateScreen = () => {
   const [err, setErr] = useState(null);
   const startWatching = async () => {
     try {
       const { granted } = await requestForegroundPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000, // Update location either every second
+          distanceInterval: 10, // or every 10 meters
+        },
+        (location) => console.log(location)
+      );
       if (!granted) {
         throw new Error('Location permission not granted');
       }
