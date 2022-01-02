a04bc0403c5dd83bfb3a67f18b36c9f5b5e593e1
Daumantas <daumis05@gmail.com>
2022-01-02 15:53:27 +0200

    271. Listing all tracks

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import { Button, Text, ListItem } from 'react-native-elements';
+import { Context as TrackContext } from '../context/TrackContext';
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext);
+  console.log(state);
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {});
+    fetchTracks();
+    return unsubscribe;
+  }, [navigation]);
   return (
     <SafeAreaView style={styles.container}>
       <Text style={{ fontSize: 48 }}>Track List Screen</Text>
-      <Button title="Go to Track Detail" onPress={() => navigation.navigate('TrackDetail')} />
+      <FlatList
+        data={state}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity>
+              <ListItem>
+                <ListItem.Content>
+                  <ListItem.Title>{item.name}</ListItem.Title>
+                </ListItem.Content>
+                <ListItem.Chevron />
+              </ListItem>
+            </TouchableOpacity>
+          );
+        }}
+      />
     </SafeAreaView>
   );
 };
