54db3ce5df37c3e80c1592cd556e3a8a9720b09d
Daumantas <daumis05@gmail.com>
2022-01-02 14:20:54 +0200

    261. Refactor code and move helper functions with dependencies to useEffect body

--- tracks/src/hooks/useLocation.js
+++ tracks/src/hooks/useLocation.js
@@ -3,32 +3,35 @@ import { Accuracy, requestForegroundPermissionsAsync, watchPositionAsync } from 
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
-  const [subscriber, setSubsriber] = useState(null);
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync();
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000, // Update location either every second
-          distanceInterval: 10, // or every 10 meters
-        },
-        callback // we might want to change what useLocation does on receivign location, so will pass the callback from other components
-      );
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
-      setSubsriber(sub);
-    } catch (e) {
-      setErr(e);
-    }
-  };
+
   useEffect(() => {
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        const { granted } = await requestForegroundPermissionsAsync();
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000, // Update location either every second
+            distanceInterval: 10, // or every 10 meters
+          },
+          callback // we might want to change what useLocation does on receivign location, so will pass the callback from other components
+        );
+        if (!granted) {
+          throw new Error('Location permission not granted');
+        }
+      } catch (e) {
+        setErr(e);
+      }
+    };
+
     if (shouldTrack) {
       startWatching();
     } else {
-      subscriber.remove();
-      setSubsriber(null);
+      if (subscriber) {
+        subscriber.remove();
+      }
+      subscriber = null;
     }
     return () => {
       if (subscriber) subscriber.remove(); // return a "cleanup" function - every time the useEffect is rerun, this will clean the previous version of it
